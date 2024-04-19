0310e56ea4e841f1690282098020f33a649a9c7c
Afeez Akande <101337556+Freezyhub@users.noreply.github.com>
2024-04-19 18:50:19 -0400

    Add files via upload

---
+++ Day 29 ( Date and Time functions).sql
@@ -0,0 +1,22 @@
+
+-- Day 29 ( Date and Time functions)
+
+
+
+Understanding the date and time functions is like having a secret weapon in your analytics arsenal. With the functions, we can unravel temporal patterns, filter data like pros, crunch numbers accurately, aggregate insights seamlessly, and supercharge our forecasting game! But here's the real kicker: by harnessing date and time data, we're not just analyzing numbers, we're decoding trends, understanding user behavior, and making smarter decisions. So, let's roll up our sleeves and get familiar with date and time functions in the next couple of days. There are quite a few date/time functions which can be categories base on their usage:
+
+·        Date and Time Retrieval Functions
+
+·        Date Arithmetic Functions
+
+·        Date Formatting Functions
+
+·        Date Extraction Functions
+
+·        Date Conversion Functions
+
+Integrating date and time into data analysis tasks provides valuable insights into how variables evolve over time, leading to more informed decision-making and strategic planning.
+
+
+
+#100daysproblemsolvingchallenge #sql #dataanalysisskills 
